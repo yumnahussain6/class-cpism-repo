0e605c7cddaf9a8b889eaeef6743d1c5fa6215e9
yumnahussain6 <yumna.hosain@gmail.com>
2024-11-23 03:05:29 -0800

    m

---
+++ js/userinput.js
@@ -0,0 +1,5 @@
+let username = prompt("Enter your Name: ");
+let userage = prompt("Enter your Age: ");
+let fathername = prompt("Enter your Father's Name: ");
+let qualification = prompt("Enter your Qualification: ");
+let city = prompt("Enter your City: ");
