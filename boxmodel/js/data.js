b0918215daf7746e5413908cb1f60cd0b3b60822
yumnahussain6 <yumna.hosain@gmail.com>
2025-01-04 03:53:56 -0800

    url

--- boxmodel/js/data.js
+++ boxmodel/js/data.js
@@ -3,12 +3,28 @@ $(document).ready(function(){
         url:"boxmodel/json/data.json",
         type:"get",
         success: function(data1){
-            let i="";
-          $.each(data1, function(a,b){
-            console.log(b)
-            i+=`<li>${a+" "+b.name}</li>`;
+          //   let i="";
+          // $.each(data1, function(a,b){
+          //   console.log(b)
+          //   i+=`<li>${a+" "+b.name}</li>`;
+          // })
+          // $("#x").html(i);
+
+          $.each(data1,function(indexes,objects){
+            let catObj = objects.name.split(" ").join("").toLowerCase();
+            let i ="";
+            i+=`<div class="col">
+        <div class="col-lg-3 col-md-6 mt-2" style="padding-left: 8px;">
+            <div class="card">
+                <img src="${objects.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <a href="product.html?category=${catObj}" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>`
           })
-          $("#x").html(i);
+          $("#category").html("i");
         }
     })
 })
